a760d6947d73dd960d68a4601022cb004e7b7e05
Maksym <maksym.myronov2018@gmail.com>
2024-06-17 23:44:40 +0300

    [TA-0005] - add search information and calendar

--- src/pages/Home/components/Search/index.tsx
+++ src/pages/Home/components/Search/index.tsx
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAppDispatch, useAppSelector } from 'hooks/useStore';
+import { fetchCityData } from 'store/searchSlice';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 // Images
 import peopleIcon from 'assets/img/fontisto_male.svg';
 import searchIcon from 'assets/img/akar-icons_search.svg';
@@ -7,6 +11,30 @@ import calendarIcon from 'assets/img/akar-icons_calendar.svg';
 import s from './index.module.scss';
 
 export const Search: React.FC = () => {
+	const [city, setCity] = useState<string>('');
+	const [dateRange, setDateRange] = useState<[Date | null, Date | null]>([
+		null,
+		null
+	]);
+	const [startDate, endDate] = dateRange;
+
+	const handleDateChange = (update: [Date | null, Date | null]) => {
+		setDateRange(update);
+	};
+
+	const handleSelect = (date: Date | null) => {
+		if (date) {
+			console.log(date);
+		}
+	};
+	const dispatch = useAppDispatch();
+	const data = useAppSelector((state) => state.search);
+	console.log(data);
+
+	useEffect(() => {
+		dispatch(fetchCityData(city));
+	}, [dispatch, city]);
+
 	return (
 		<div className={s.search}>
 			<div className={s.search__container}>
@@ -16,16 +44,22 @@ export const Search: React.FC = () => {
 						type="search"
 						placeholder="Search destinations, hotels"
 						className={s.search__input}
+						value={city}
+						onChange={(e) => setCity(e.target.value)}
 					/>
 				</div>
 				<div className={s.search__container__info}>
 					<div className={s.search__block}>
 						<img src={calendarIcon} alt="calendarIcon" />
-						<p>Check in</p>
-					</div>
-					<div className={s.search__block}>
-						<img src={calendarIcon} alt="calendarIcon" />
-						<p>Check out</p>
+						<DatePicker
+							selectsRange
+							startDate={startDate ?? undefined}
+							endDate={endDate ?? undefined}
+							onChange={handleDateChange}
+							onSelect={handleSelect}
+							isClearable
+							className={s.search__container__datapicker}
+						/>
 					</div>
 					<div className={s.search__block}>
 						<img src={peopleIcon} alt="peopleIcon" />
---
+++ src/store/searchSlice.ts
@@ -0,0 +1,66 @@
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+interface Coordinates {
+	lat: number;
+	lon: number;
+}
+
+interface City {
+	name: string;
+	country_name: string;
+	coordinates: Coordinates;
+}
+
+interface SearchState {
+	city: City[];
+	status: 'idle' | 'loading' | 'succeeded' | 'failed';
+	error: string | null;
+}
+
+const initialState: SearchState = {
+	city: [],
+	status: 'idle',
+	error: null
+};
+
+export const fetchCityData = createAsyncThunk(
+	'search/fetchCityData',
+	async (term: string) => {
+		const response = await axios.get(
+			'https://autocomplete.travelpayouts.com/places2',
+			{
+				params: {
+					locale: 'en',
+					types: ['airport', 'city'],
+					term
+				}
+			}
+		);
+		return response.data;
+	}
+);
+
+const searchSlice = createSlice({
+	name: 'search',
+	initialState,
+	reducers: {},
+	extraReducers: (builder) => {
+		builder
+			.addCase(fetchCityData.pending, (state) => {
+				state.status = 'loading';
+			})
+			.addCase(
+				fetchCityData.fulfilled,
+				(state, action: PayloadAction<City[]>) => {
+					state.status = 'succeeded';
+					state.city = action.payload;
+				}
+			)
+			.addCase(fetchCityData.rejected, (state) => {
+				state.status = 'failed';
+			});
+	}
+});
+
+export default searchSlice.reducer;
